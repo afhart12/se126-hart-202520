fd9f72de9bd0a227ca2ed0f30ac6b4d8c906d4fd
afhart12 <afhart@email.neit.edu>
2025-01-11 20:44:35 -0500

    documented final version!!!
    i forgot to document it im sorry

--- week 1/lab1.py
+++ week 1/lab1.py
@@ -6,6 +6,12 @@
 # Program Prompt: a program that determines whether a meeting room is in violation of fire regulations regarding the maximum room capacity. The program will accept the maximum room capacity and the number of people attending the meeting. If the number of people is less than or equal to the maximum room capacity, the program announces that it is legal to hold the meeting and tells how many additional people may legally attend. If the number of people exceeds the maximum room capacity, the program announces that the meeting cannot be held as planned due to the fire regulation and tells how many people must be excluded in order to meet the fire regulations. The user should be allowed to enter and check as many rooms as they would like without exiting the program.
 
 # Variable dictionary:
+# answer = value that runs the main loop, determines if the user will enter more data or not
+# maxCapacity = the maximum capacity of the meeting room entered by the user
+# pplAttending = the amount of people attending the meeting entered by the user
+# dif = the value that holds the difference of maxCapacity - pplAttending to determine the amount of people over/under capacity
+
+
 
 answer = "y"
 maxCapacity = 0
