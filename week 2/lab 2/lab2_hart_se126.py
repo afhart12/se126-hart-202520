c3ea30d75a9b55f12d063146f16dd83822b23b7a
afhart12 <afhart@email.neit.edu>
2025-01-16 20:04:58 -0500

    lab 2 finished

--- week 2/lab 2/lab2_hart_se126.py
+++ week 2/lab 2/lab2_hart_se126.py
@@ -10,24 +10,39 @@
 # record[0] is type. record[1] is brand. record[2] is CPU. record[3] is RAM.
 # record[4] is 1st disk, record[5] is no HDD, record[6] is either 2nd disk or OS, record[7] is either OS or YR
 # record[8] is YR(if it exists)
+# totalRecords = a counter for how many computers are listed in the file
+# num = the number associated with record 5, indicating if there is 1 or 2 hard drives
+# disk2 = record 6 if applicable
+# os = operating system, record 6 or 7
+# yr = year, record 7 or 8
 
 import csv
 
 with open("week 2/lab 2/filehandling.csv") as csvfile:
 
     file = csv.reader(csvfile)
+    totalRecords = 0
 
-    print("{'Type':7} {'Brand':7} {'CPU':3} {'RAM':3} {'1st Disk':6} {'No HDD':2} {'2nd Disk':6} {'OS':4} {'YR':3}")
+    print(f"{'Type':7} {'Brand':7} {'CPU':3} {'RAM':3} {'1st Disk':6} {'No HDD':4} {'2nd Disk':10} {'OS':4} {'YR':3}")
     for record in file:
+        totalRecords += 1
         if record[0] == "D":
             record[0] = "Desktop"
-        else: record[0] == "Laptop"
-        float(record[5])
-        if record[5] == 1:
-            os = record[6]
-        else: os = record[7]
+        elif record[0] == "L": record[0] == "Laptop"
+        num = int(record[5])
+        os = record[6]
+        yr = record[7]
+        if num == 2:
+            disk2 = record[6]
+            os = record[7]
+            yr = record[8]
+        else: disk2 = "n/a"
         if record[1] == "DL":
             record[1] = "Dell"
         elif record[1] == "GW":
             record[1] = "Gateway"
+        print(f"{record[0]:7} {record[1]:7} {record[2]:3} {record[3]:3} {record[4]:10} {record[5]:7} {disk2:6} {os:4} {yr:3}")
+
+    print(f"TOTAL COMPUTERS: {totalRecords}")
+    
     
