34db541ade70f7f1a812ce43caa5ba03c6c4395e
afhart12 <afhart@email.neit.edu>
2025-01-30 16:48:02 -0500

    better version

--- week 3/lab3.py
+++ week 3/lab3.py
@@ -17,27 +17,30 @@ votedCount = 0
 
 num = []
 age =[]
-
+reg = []
+votes = []
 
 with open("week 3/voters_202040.csv") as csvfile:
     file = csv.reader(csvfile)
 
     for record in file:
-        num = record[0]
-        age = int(record[1])
-        reg = record[2]
-        votes = record[3]
+        num.append(record[0])
+        age.append(int(record[1]))
+        reg.append(record[2])
+        votes.append(record[3])
         recordCount += 1
-        if age < 18:
-            notEligCount += 1
-        elif age >= 18 and reg == "N":
-            notRegCount += 1
-        elif reg == "Y" and votes == "N":
-            noVoteCount += 1
-        elif votes == "Y":
-            votedCount += 1
-    print(f"Records Processed: {recordCount}")
-    print(f"Not Able to Register: {notEligCount}")
-    print(f"Old Enough to Vote, Not Registered: {notRegCount}")
-    print(f"Eligible, Didn't Vote: {noVoteCount}")
-    print(f"Voted: {votedCount}")
+
+for index in range(0, len(age)):
+    if age[index] < 18:
+        notEligCount += 1
+    elif age[index] >= 18 and reg[index] == "N":
+        notRegCount += 1
+    elif reg[index] == "Y" and votes[index] == "N":
+        noVoteCount += 1
+    elif votes[index] == "Y":
+        votedCount += 1
+print(f"Records Processed: {recordCount}")
+print(f"Not Able to Register: {notEligCount}")
+print(f"Old Enough to Vote, Not Registered: {notRegCount}")
+print(f"Eligible, Didn't Vote: {noVoteCount}")
+print(f"Voted: {votedCount}")
