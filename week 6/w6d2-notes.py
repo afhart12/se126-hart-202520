b3c1d70ea20049f60ec7733ace4a779b6a5ac3ad
afhart12 <afhart@email.neit.edu>
2025-02-13 16:28:18 -0500

    demo started

---
+++ week 6/w6d2-notes.py
@@ -0,0 +1,8 @@
+# binary search - separating  a collection into 2 halves, low and high
+# - can only find ONE UNIQUE value, no duplicates
+# - can only be performed on ORDERED collections
+# ordering: ascending(low to high), descending(high to low)
+# alphabet - ascending - aAbBcCdD... and so on
+# minimum will always start at 0 in this class
+# max will always be the length of the list minus 1
+# ALWAYS SORT BEFORE SEARCH!!! DONT DO SEARCH FIRST
