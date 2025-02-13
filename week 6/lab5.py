a563826e968772b2810bb49c0c21df4dd82053fb
afhart12 <afhart@email.neit.edu>
2025-02-13 16:44:04 -0500

    overview

---
+++ week 6/lab5.py
@@ -0,0 +1,9 @@
+# library menu:
+# 1 - all data, print statement
+# 2 - title keyword, sequential search
+# 3 - author, sequential
+# 4 - genre, sequential
+# 5 - one specific library number item, binary search
+# 6 - sequential, status of avaliable
+# 7 - status of on loan, sequential
+# 8 - exit the loop
