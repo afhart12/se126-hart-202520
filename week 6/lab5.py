127954d080fbe9e37c947f996d60f86e36c03c31
afhart12 <afhart@email.neit.edu>
2025-02-14 21:45:45 -0500

    updated

--- week 6/lab5.py
+++ week 6/lab5.py
@@ -16,37 +16,23 @@
 # 8 - exit the loop
 
 # variable dictionary
-
+import csv
 libnum = []
 title = []
 author = []
 genre = []
 pgcount = []
 status = []
-
+found = []
 menu = "y" # this is to start the main while loop
-# functions -----
-def display(x, records):
-    print(f"{'NUMBER':7}  {'TITLE':32}  {'AUTHOR':25}  {'GENRE':15} {'PG COUNT':4} {'STATUS':9}")
-    print("----------------------------------------------------------------")
-    if x != "x":
-        #printing one record
-        print(f"{libnum[x]:7}  {title[x]:32}  {author[x]:25}  {genre[x]:15} {pgcount[x]:4} {status[x]:9}")
+def swap(i, listName):
+    temp = listName[i]
+    listName[i] = listName[i + 1]
+    listName[i + 1] = temp
 
-    elif found:
-        #printing multiples, based on length stored in 'foundList'
-        for i in range(0, records):
-            print(f"{libnum[x]:7}  {title[x]:32}  {author[x]:25}  {genre[x]:15} {pgcount[x]:4} {status[x]:9}") 
-    
-    else:
-        #printing full data, based on length stored in 'records'
-        for i in range(0, records):
-            print(f"{libnum[x]:7}  {title[x]:32}  {author[x]:25}  {genre[x]:15} {pgcount[x]:4} {status[x]:9}")
-
-    print("----------------------------------------------------------------\n")
+print("----------------------------------------------------------------\n")
 
 #-------
-import csv
 
 with open("week 6/book_list.csv") as csvfile:
     file = csv.reader(csvfile)
@@ -55,7 +41,10 @@ with open("week 6/book_list.csv") as csvfile:
         libnum.append(rec[0])
         title.append(rec[1])
         author.append(rec[2])
-        
+        genre.append(rec[3])
+        pgcount.append(rec[4])
+        status.append(rec[5])
+
 
 while menu == "y":
     print("Welcome to the library menu!")
@@ -68,5 +57,107 @@ while menu == "y":
     print("7. Show all On Loan Books")
     print("8. EXIT")
     opt = input("Select a number(1/2/3/4/5/6/7/8): ")
-    if opt == "1":
-        display("x", len(libnum) - 1)
+
+    if opt not in ["1", "2", "3", "4", "5", "6", "7", "8"]:
+        print("INVALID ENTRY!!! Please try again.")
+
+    elif opt == "1":
+        for i in range(0, len(libnum)):
+            print(f"{libnum[i]:7}  {title[i]:32}  {author[i]:25}  {genre[i]:15} {pgcount[i]:4} {status[i]:9}")
+
+    elif opt == "2":
+        found = []
+        search = input("What title are you searching for?: ").lower()
+
+        for i in range(0, len(title)):
+            if search in title[i].lower():
+                found.append(i)
+
+        if not found:
+            print(f"Sorry, your search for {search} was not found :[")
+        
+        else:
+            print(f"Your search for {search} has been found!")
+            for i in range(0, len(found)):
+                print(f"{libnum[found[i]]:7}  {title[found[i]]:32}  {author[found[i]]:25}  {genre[found[i]]:15} {pgcount[found[i]]:4} {status[found[i]]:9}")
+    
+    elif opt == "3":
+        found = []
+
+        search = input("Which author are you searching for?: ")
+
+        for i in range(0, len(author)):
+            if search.lower() == author[i].lower():
+                found.append(i)
+            
+        if not found:
+            print(f"Sorry, your search for {search} was not found.")
+
+        else:
+            print(f"Your search for {search} has been found!")
+            for i in range(0, len(found)):
+                print(f"{libnum[found[i]]:7}  {title[found[i]]:32}  {author[found[i]]:25}  {genre[found[i]]:15} {pgcount[found[i]]:4} {status[found[i]]:9}")
+
+    elif opt == "4":
+        found = []
+
+        search = input("What genre are you searching for?: ")
+
+        for i in range(0, len(genre)):
+            if search.lower() == genre[i].lower():
+                found.append(i)
+
+        if not found:
+            print(f"Sorry, your search for {search} was not found :[")
+        
+        else: 
+            print(f"Your search for {search} has been found!")
+            for i in range(0, len(found)):
+                print(f"{libnum[found[i]]:7}  {title[found[i]]:32}  {author[found[i]]:25}  {genre[found[i]]:15} {pgcount[found[i]]:4} {status[found[i]]:9}")
+
+    elif opt == "5":
+
+# performing swap here
+        for i in range(0, len(libnum) - 1):
+            for j in range(0, len(libnum) -1): 
+                if libnum[j] > libnum[j + 1]:
+                    #they must swap places because the higher value must come afterwards
+                    temp = libnum[j] # temporary variable
+                    libnum[j] = libnum[j + 1]
+                    libnum[j + 1] = temp
+
+                    swap(j, title)
+                    swap(j, author)
+                    swap(j, genre)
+                    swap(j, pgcount)
+                    swap(j, status)
+
+        min = 0
+        max = len(libnum)
+        mid = int((min + max) / 2)
+
+        while min < max and search.lower() != libnum[mid].lower():
+            if search.lower() < libnum[mid].lower():
+                max = mid - 1
+            else:
+                #search > name[mid]
+                min = mid + 1
+
+            mid = int((min + max) / 2)
+
+        if search.lower() == libnum[mid].lower():
+            print(f"Your search for {search} has been found!")
+            print(f"{libnum[mid]:7}  {title[mid]:32}  {author[mid]:25}  {genre[mid]:15} {pgcount[mid]:4} {status[mid]:9}")
+
+        else:
+            print(f"Your search for {search} was not found :[")
+
+
+    elif opt == "6":
+
+        found = []
+
+        search = "Avaliable"
+            
+
+
